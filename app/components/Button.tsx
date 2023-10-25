44e4d55634c62badbd427591f109199bebe72b1d
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-25 20:22:49 -0300

    feat: adding all props except label to button on button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,16 +2,17 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
-export const Button: React.FC<Props> = ({ label }) => (
+export const Button: React.FC<Props> = ({ label, ...props }) => (
   <button
     style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
     className={`
         bg-[#FFEC65] hover:filter hover:brightness-125 transition-all
-        text-black font-bold text-[1.75rem] uppercase
+        text-black font-bold text-[1.25rem] uppercase
         active:transform active:translate-y-1
         flex justify-center items-center 
-        w-[15rem] h-[5rem] rounded-full
+        w-[12rem] h-[4rem] rounded-full
     `}
+    {...props}
   >
     {label}
   </button>
