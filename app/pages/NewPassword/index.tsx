b863f51387298f4975a9492a63592398e27be4eb
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-25 20:24:00 -0300

    feat: creating change password  page

---
+++ app/pages/NewPassword/index.tsx
@@ -0,0 +1,25 @@
+import { InputWithLabel, Button } from "@/app/components";
+
+export const NewPassword: React.FC = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-start w-full h-full gap-[2rem]">
+        <InputWithLabel
+          label="Senha"
+          placeholder="Preencha sua nova senha"
+          password
+        />
+
+        <InputWithLabel
+          label="Senha"
+          placeholder="Confirme sua nova senha"
+          password
+        />
+      </div>
+
+      <div className="absolute flex items-center justify-around w-[79%] h-[5rem] bottom-[3rem]">
+        <Button label="Salvar" />
+      </div>
+    </>
+  );
+};
