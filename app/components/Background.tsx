9a6acfaa34d12d50c00a2095b738ee72fa959142
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-25 19:42:18 -0300

    adjusting background cursor type

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export const Background = ({ children }: Props) => (
   <div
-    className="fixed inset-0 h-screen w-screen bg-cover bg-center"
+    className="fixed inset-0 h-screen w-screen bg-cover bg-center cursor-auto"
     style={{ backgroundImage: "url(/background.png)" }}
   >
     <Image
-      className="absolute !bottom-0 !-left-[22rem] !w-[75%] !h-[75%] !top-[unset] z-[-1]"
+      className="absolute !bottom-0 !-left-[22rem] !w-[75%] !h-[75%] !top-[unset] z-[-1] cursor-auto"
       alt="illustration of two doctors in medical uniforms"
       src="/undraw_doctors_p6aq 1.svg"
       objectFit="contain"
