deed2a30cd27286d95c7f122a724a9f4a7fc30a8
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-26 18:57:10 -0300

    feat: allowing extra styling on Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,15 +4,16 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button: React.FC<Props> = ({ label, ...props }) => (
   <button
-    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+    {...props}
+    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)", ...props.style }}
     className={`
         bg-[#FFEC65] hover:filter hover:brightness-125 transition-all
         text-black font-bold text-[1.25rem] uppercase
         active:transform active:translate-y-1
         flex justify-center items-center 
         w-[12rem] h-[4rem] rounded-full
+        ${props.className}
     `}
-    {...props}
   >
     {label}
   </button>
