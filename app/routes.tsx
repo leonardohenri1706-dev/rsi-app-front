e9d93aa4c1cb88e752ab79b7a25083a005071cd8
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 11:08:26 -0300

    feat: adding indexed and indexName params to routes to better control indexing

--- app/routes.tsx
+++ app/routes.tsx
@@ -2,6 +2,8 @@ import * as Pages from "./pages";
 
 interface RouteProps {
   Component: React.PropsWithChildren<any>;
+  indexName?: string;
+  indexed?: boolean;
   name: string;
   path: string;
 }
@@ -31,46 +33,59 @@ const pages: RouteProps[] = [
     Component: Pages.PatientType,
     name: "Tipo de paciente",
     path: "/patient-type",
+    indexed: true,
   },
   {
     Component: Pages.NotICURespiratoryTractCheckUp1,
+    indexName: "Avaliação das vias aéreas não UTI 1",
     name: "Avaliação das vias aéreas",
     path: "/not-icu-respiratory-tract-check-up-1",
+    indexed: true,
   },
   {
     Component: Pages.NotICURespiratoryTractCheckUp2,
+    indexName: "Avaliação das vias aéreas não UTI 2",
     name: "Avaliação das vias aéreas",
     path: "/not-icu-respiratory-tract-check-up-2",
+    indexed: true,
   },
   {
     Component: Pages.ICURespiratoryTractCheckUp1,
+    indexName: "Avaliação das vias aéreas UTI 1",
     name: "Avaliação das vias aéreas",
     path: "/icu-respiratory-tract-check-up-1",
+    indexed: true,
   },
   {
     Component: Pages.ICURespiratoryTractCheckUp2,
+    indexName: "Avaliação das vias aéreas UTI 2",
     name: "Avaliação das vias aéreas",
     path: "/icu-respiratory-tract-check-up-2",
+    indexed: true,
   },
   {
     Component: Pages.Positioning,
     name: "Posicionamento",
     path: "/positioning",
+    indexed: true,
   },
   {
     Component: Pages.PreOxygenation,
     name: "Pré-oxigenação",
     path: "/pre-oxygenation",
+    indexed: true,
   },
   {
     Component: Pages.RapidSequenceInduction,
     name: "Indução em sequência rápida",
     path: "/rapid-sequence-induction",
+    indexed: true,
   },
   {
     Component: Pages.AwakeIntubation,
     name: "Intubação acordado",
     path: "/awake-intubation",
+    indexed: true,
   },
 ];
 
