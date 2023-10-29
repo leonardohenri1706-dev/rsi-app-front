fd3fdcc9f27b533cdd3614e2952a3cc7975d5d62
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 09:24:11 -0300

    feat: creating PreOxygenation page

---
+++ app/pages/PreOxygenation/index.tsx
@@ -0,0 +1,22 @@
+import { BaseLayout, Button } from "@/app/components";
+import { useRoutes } from "@/app/hooks";
+
+export const PreOxygenation: React.FC = () => {
+  const { goToPositioning } = useRoutes();
+
+  return (
+    <BaseLayout.Root>
+      <BaseLayout.Content className="!justify-around !h-[80%] !pt-[0.5rem]">
+        <video autoPlay controls className="!w-full !h-full">
+          <source src="/pre-oxygenation.mp4" type="video/mp4" />
+        </video>
+      </BaseLayout.Content>
+
+      <BaseLayout.Buttons>
+        <Button onClick={goToPositioning} label="Voltar" />
+
+        <Button onClick={() => {}} label="Próximo" />
+      </BaseLayout.Buttons>
+    </BaseLayout.Root>
+  );
+};
