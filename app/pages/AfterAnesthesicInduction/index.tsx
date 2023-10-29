6360b92d69e861b0fd9c2f1c350a328bb435bb78
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 15:47:14 -0300

    fix: creating AfterAnesthesicInduction

---
+++ app/pages/AfterAnesthesicInduction/index.tsx
@@ -0,0 +1,39 @@
+import { BaseLayout, Button, FlowChartQuestion } from "@/app/components";
+import { useRoutes } from "@/app/hooks";
+
+import { flowChartOptionsButtonsClassName } from "..";
+
+export const AfterAnesthesicInduction: React.FC = () => {
+  const { goBack, goToLaryngealMask } = useRoutes();
+
+  return (
+    <BaseLayout.Root>
+      <BaseLayout.Content className="!justify-around !h-[75%]">
+        <p className="text-[1.5rem]">
+          Intubação traqueal – máximo 3 tentativas (videolaringoscópio,
+          laringoscópio tradicional, MacCoy). Capnógrafo disponível?​
+        </p>
+
+        <FlowChartQuestion question="Sucesso?" />
+
+        <div className="flex flex-row justify-center items-center gap-[3rem]">
+          <Button
+            className={flowChartOptionsButtonsClassName}
+            onClick={() => alert("Respirador")} // TODO: Understand and implement this part
+            label="Sim"
+          />
+
+          <Button
+            className={flowChartOptionsButtonsClassName}
+            onClick={goToLaryngealMask}
+            label="Não"
+          />
+        </div>
+      </BaseLayout.Content>
+
+      <BaseLayout.Buttons>
+        <Button onClick={goBack} label="Voltar" />
+      </BaseLayout.Buttons>
+    </BaseLayout.Root>
+  );
+};
