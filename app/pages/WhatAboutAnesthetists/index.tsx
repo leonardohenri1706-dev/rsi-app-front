80799b7939d3feb707280f732918ab565ce42c7e
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 15:49:53 -0300

    feat: creating WhatAboutAnesthetists

---
+++ app/pages/WhatAboutAnesthetists/index.tsx
@@ -0,0 +1,26 @@
+import { BaseLayout, Button } from "@/app/components";
+import { useRoutes } from "@/app/hooks";
+
+export const WhatAboutAnesthetists: React.FC = () => {
+  const { goBack } = useRoutes();
+
+  return (
+    <BaseLayout.Root>
+      <BaseLayout.Content className="!justify-around !h-[80%] !pt-[1rem] !pb-[3rem]">
+        <div className="flex flex-row h-full w-full text-black text-[1.25rem] items-center justify-center">
+          <p className="text-black">
+            Não há grandes diferenças, apenas em cirurgias programadas a
+            intubação pode ser cancelada (diferente de insuficiência
+            respiratória, parada cardíaca, etc) através da reversão de drogas
+            utilizadas. A indução em sequência rápida não é obrigatória em
+            pacientes compensados
+          </p>
+        </div>
+      </BaseLayout.Content>
+
+      <BaseLayout.Buttons>
+        <Button onClick={goBack} label="Voltar" />
+      </BaseLayout.Buttons>
+    </BaseLayout.Root>
+  );
+};
