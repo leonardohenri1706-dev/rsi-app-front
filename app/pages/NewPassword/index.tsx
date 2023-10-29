243f1596434dadc50916c2aadba152703baca846
Romulo Iorio <romuloiorio@hotmail.com>
2023-10-29 17:53:51 -0300

    feat: adding states to NewPassword

--- app/pages/NewPassword/index.tsx
+++ app/pages/NewPassword/index.tsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
+
 import { InputWithLabel, BaseLayout, Button } from "@/app/components";
 import { useRoutes } from "@/app/hooks";
 
 export const NewPassword: React.FC = () => {
   const { goToLogin } = useRoutes();
 
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+
   return (
     <BaseLayout.Root>
       <BaseLayout.Content>
         <InputWithLabel
-          label="Senha"
           placeholder="Preencha sua nova senha"
+          onChange={setPassword}
+          value={password}
+          label="Senha"
           password
         />
 
         <InputWithLabel
-          label="Confirmação da senha"
           placeholder="Confirme sua nova senha"
+          onChange={setPasswordConfirmation}
+          value={passwordConfirmation}
+          label="Confirmação da senha"
           password
         />
       </BaseLayout.Content>
