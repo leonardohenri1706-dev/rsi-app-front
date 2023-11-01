27d0ed36ea43795155bf7c5447f12f285b153f9b
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-01 20:03:29 -0300

    feat: creating TableRow

---
+++ app/pages/RespiratoryTractCheckUpMacocha/TableRow.tsx
@@ -0,0 +1,29 @@
+interface Props {
+  Input?: JSX.Element;
+  centered?: boolean;
+  parameter: string;
+  score?: string;
+}
+
+export const TableRow: React.FC<Props> = ({
+  parameter,
+  score,
+  Input,
+  centered,
+}) => {
+  const [identifier, rest] = parameter.split(":");
+
+  const conditionalCentered = centered ? "text-center" : "";
+
+  return (
+    <tr key={parameter} className="border-b border-black py-1">
+      <td
+        className={`w-1/2 pl-[2rem] border-r border-black ${conditionalCentered}`}
+      >
+        <b>{identifier}:</b>
+        {rest ?? ""}
+      </td>
+      <td className="w-1/2 text-center">{score ?? Input}</td>
+    </tr>
+  );
+};
