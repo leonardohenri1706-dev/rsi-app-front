9872c4f2dd87fa2fc176841afac5a2dbb769e945
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 20:42:43 -0300

    feat: creating LoginUseCase

---
+++ app/api-modules/applications/LoginUseCase.ts
@@ -0,0 +1,49 @@
+import type { CryptoService, JwtService, UsersRepository } from "../infra";
+import type { LoginBody } from "../presentation/validators";
+
+interface Return {
+  token: string;
+}
+
+export class LoginUseCase {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly cryptoService: CryptoService,
+    private readonly JwtService: JwtService
+  ) {}
+
+  public async execute(input: LoginBody): Promise<Return> {
+    const { password, email } = input;
+
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw {
+        cause: "User with this email does not exist",
+        type: "USER_DOES_NOT_EXIST",
+        status: 404,
+      };
+    }
+
+    const hashedPassword = this.cryptoService.createHash(
+      password,
+      user.cryptographic_salt
+    );
+
+    const isPasswordValid = hashedPassword === user.password;
+
+    if (!isPasswordValid) {
+      throw {
+        cause: "Invalid password",
+        type: "INVALID_PASSWORD",
+        status: 401,
+      };
+    }
+
+    const token = this.JwtService.generateToken({
+      user_id: String(user._id),
+      email: user.email,
+    });
+    return { token };
+  }
+}
