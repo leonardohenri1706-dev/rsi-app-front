60ba56cf13ac8150f2c4e6ad1314d548f967526f
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 20:48:48 -0300

    feat: creating RegistrationValidator

---
+++ app/api-modules/presentation/validators/RegistrationValidator.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import {
+  IntubationExperience,
+  UserProfession,
+} from "../../domain/entities/User";
+
+import { Validator } from "../routers";
+
+const professionEnum = Object.values(UserProfession).map((value) =>
+  String(value)
+);
+const intubationExperienceEnum = Object.values(IntubationExperience).map(
+  (value) => String(value)
+);
+
+const bodyValidation = z.object({
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string(),
+  profession: z.enum([professionEnum[0], ...professionEnum]),
+  intubationExperience: z.enum([
+    intubationExperienceEnum[0],
+    ...intubationExperienceEnum,
+  ]),
+});
+
+export type RegistrationBody = z.infer<typeof bodyValidation>;
+
+export class RegistrationValidator implements Validator {
+  public validate(body: RegistrationBody) {
+    const parsed = bodyValidation.safeParse(body);
+    if (!parsed.success) throw parsed.error;
+  }
+}
