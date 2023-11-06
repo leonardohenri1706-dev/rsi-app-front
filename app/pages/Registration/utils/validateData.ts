2f9d3d1315d8a22c78826430cc7cf825ef016118
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 23:27:57 -0300

    feat: creating validateData

---
+++ app/pages/Registration/utils/validateData.ts
@@ -0,0 +1,31 @@
+import { RegistrationBody } from "@/app/services/api/registration";
+
+export const validateData = (data: RegistrationBody | null) => {
+  const errors: Partial<Record<keyof RegistrationBody, string>> = {};
+
+  if (!data?.email) errors.email = "E-mail é obrigatório";
+
+  if (!data?.name) errors.name = "Nome é obrigatório";
+
+  if (!data?.password) errors.password = "Senha é obrigatória";
+
+  if (!data?.passwordConfirmation)
+    errors.passwordConfirmation = "Confirmação de senha é obrigatória";
+
+  if (
+    data?.password &&
+    data?.passwordConfirmation &&
+    data?.password !== data?.passwordConfirmation
+  )
+    errors.passwordConfirmation = "As senhas não conferem";
+
+  if (data?.password && data?.password.length < 6)
+    errors.password = "A senha deve ter no mínimo 6 caracteres";
+
+  if (!data?.intubation_experience)
+    errors.intubation_experience = "Experiência em intubação é obrigatória";
+
+  if (!data?.profession) errors.profession = "Profissão é obrigatória";
+
+  return errors;
+};
