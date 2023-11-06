c86c08d8add943f301e6cf2258290e0e9f79b981
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-05 22:48:37 -0300

    feat: creating api index

---
+++ app/services/api/api.ts
@@ -0,0 +1,21 @@
+import type { AxiosResponse } from "axios";
+import axios from "axios";
+
+const apiUrl =
+  process.env.NEXT_PUBLIC_API_URL || "https://rsi-app-seven.vercel.app";
+const authentication = process.env.NEXT_PUBLIC_API_AUTHENTICATION;
+
+const defaultOptions = {
+  headers: { "Content-Type": "application/json" },
+  baseURL: `${apiUrl}/api`,
+};
+
+const api = axios.create(defaultOptions);
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = authentication;
+
+  return config;
+});
+
+export default api;
