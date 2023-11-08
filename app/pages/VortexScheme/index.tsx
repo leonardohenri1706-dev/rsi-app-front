008c532dbf09158bb9d8bea1008211521388abda
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-08 17:47:59 -0300

    feat: adjusting styling and adding text on VortexScheme

--- app/pages/VortexScheme/index.tsx
+++ app/pages/VortexScheme/index.tsx
@@ -10,36 +10,41 @@ export const VortexScheme: React.FC = () => {
 
   return (
     <BaseLayout.Root>
-      <BaseLayout.Content className="!justify-around !h-[80%] !px-[0.5rem] relative">
-        <div className="flex flex-col w-full h-full items-center justify-center">
+      <BaseLayout.Content className="!justify-start !h-[90%] !px-[0.5rem] relative">
+        <div className="flex flex-col w-full h-[77%] items-center justify-center relative">
           <Image
             src="/vortex-scheme.png"
             alt="Esquema Vortéx"
-            height={400}
-            width={400}
+            height={350}
+            width={350}
           />
+
+          <p className={`${commonTextClassName} top-[0rem] lg:-top-[1.25rem]`}>
+            Máscara facial
+          </p>
+
+          <p
+            className={`${commonTextClassName} bottom-[5.5rem] lg:bottom-[4rem] -left-[1rem] lg:md:left-[9%] lg:rotate-[55deg]  rotate-[60deg]`}
+          >
+            Máscara laríngea
+          </p>
+
+          <p
+            className={`${commonTextClassName} bottom-[5.5rem] lg:bottom-[4rem] -right-[1rem] lg:md:right-[9%] -lg:rotate-[55deg] -rotate-[60deg]`}
+          >
+            Tubo traqueal
+          </p>
+
+          <p
+            className={`${commonTextClassName} !text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+          >
+            Traqueostomia
+          </p>
         </div>
 
-        <p className={`${commonTextClassName} top-10 lg:top-0`}>
-          Máscara facial
-        </p>
-
-        <p
-          className={`${commonTextClassName} bottom-[8.5rem] lg:bottom-[5rem] left-0 md:left-[5%] lg:rotate-[55deg]  rotate-[60deg]`}
-        >
-          Máscara laríngea
-        </p>
-
-        <p
-          className={`${commonTextClassName} bottom-[8.5rem] lg:bottom-[5rem] right-0 md:right-[5%] -lg:rotate-[55deg] -rotate-[60deg]`}
-        >
-          Tubo traqueal
-        </p>
-
-        <p
-          className={`${commonTextClassName} !text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
-        >
-          Traqueostomia
+        <p className="text-gray-500 font-bold text-[0.875rem] text-center px-[1rem] mt-2">
+          Esquema que busca usar máscara facial, IOT e máscara laríngea para
+          evitar acesso cervical
         </p>
       </BaseLayout.Content>
 
