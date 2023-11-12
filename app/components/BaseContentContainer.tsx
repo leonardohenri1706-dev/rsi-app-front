525cc03073aaab31d44ad227664fb5ef3b35ebec
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:25:38 -0300

    feat: adding style prop to BaseContentContainer

--- app/components/BaseContentContainer.tsx
+++ app/components/BaseContentContainer.tsx
@@ -1,14 +1,17 @@
 interface Props {
   children: React.ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 }
 
 export const BaseContentContainer: React.FC<Props> = ({
-  children,
   className,
+  children,
+  style,
 }) => (
   <div
     className={`flex flex-col items-center justify-start w-full h-full pt-[1rem] px-[0.5rem] md:px-[1rem] lg:px-[2rem] text-black ${className}`}
+    style={style}
   >
     {children}
   </div>
