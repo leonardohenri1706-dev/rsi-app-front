cb1483afcd048aae3ba13e525806a1cabce8a810
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 22:04:11 -0300

    feat: adjusting responsivity on Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,11 +8,11 @@ export const Button: React.FC<Props> = ({ label, ...props }) => (
     style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)", ...props.style }}
     className={`
         bg-[#FFEC65] hover:filter hover:brightness-125 transition-all
-        text-black font-bold lg:text-[1rem] uppercase
+        text-black font-bold lg:text-[1rem] text-[0.75rem] uppercase
         active:transform active:translate-y-1
         flex justify-center items-center 
         lg:w-[10rem] lg:h-[4rem] 
-        w-[8rem] h-[3rem]
+        w-[6rem] h-[3rem]
         rounded-full
         ${props.className}
     `}
