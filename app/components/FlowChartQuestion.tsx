0b7f9eaf2d44c383deed780bf13853cef26a0bb3
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 22:04:18 -0300

    feat: adjusting responsivity on FlowChartQuestion

--- app/components/FlowChartQuestion.tsx
+++ app/components/FlowChartQuestion.tsx
@@ -3,7 +3,9 @@ interface Props {
 }
 
 export const FlowChartQuestion: React.FC<Props> = ({ question }) => (
-  <div className="flex flex-col h-[10rem] w-[25rem] items-center justify-center bg-[#90C7BE77] rounded-[3rem]">
-    <p className="text-black font-bold text-[1.325rem]">{question}</p>
+  <div className="flex flex-col lg:h-[10rem] lg:w-[25rem] w-[80vw] h-[8rem] items-center justify-center bg-[#90C7BE77] rounded-[3rem]">
+    <p className="text-black font-bold lg:text-[1.325rem] text-[0.9rem]">
+      {question}
+    </p>
   </div>
 );
