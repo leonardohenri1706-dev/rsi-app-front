aacc4531be65bcbc556acddabab69b75478e21c1
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-11 21:27:12 -0300

    feat: creating RapidSequenceInductionTips component

---
+++ app/components/RapidSequenceInductionTips.tsx
@@ -0,0 +1,19 @@
+export const RapidSequenceInductionTips: React.FC = () => (
+  <div className="flex flex-col w-1/2 h-full justify-center">
+    <div className="flex flex-col w-fit h-fit justify-center gap-[2rem] bg-red-300 rounded-3xl p-3 lg:p-3">
+      <span>
+        <b>Atenção:</b>
+        <p>Reduzir doses se risco hipotensão</p>
+        <p>Hipnóticos podem ter efeito menor que 10 minutos (Refazer dose?)</p>
+      </span>
+
+      <span>
+        <b>Cuidado:</b>
+        <p>
+          Paciente que acorda e faz respiração forçada com glote fechada pode
+          fazer edema agudo de pressão negativa
+        </p>
+      </span>
+    </div>
+  </div>
+);
