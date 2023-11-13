a855303dba0c286580615d1a5fb3483ec2f5f5fe
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-12 22:16:39 -0300

    fix: using dvh to fill decive screen

--- app/components/PageLayout/index.tsx
+++ app/components/PageLayout/index.tsx
@@ -33,7 +33,7 @@ const PageLayoutWithoutContext: React.FC<Props> = ({ isPrivate, children }) => {
 
   return (
     <Background>
-      <div className="flex w-screen h-screen justify-center items-center">
+      <div className="flex w-screen lg:h-screen h-[100dvh] justify-center items-center">
         <div className="flex flex-col justify-start items-center relative w-full lg:w-[35rem] md:lg:h-[40rem] lg:h-[80%] h-[100dvh]">
           {/* <Menu /> */}
 
