bbf2c71987f9711238091f5f044ef608de4c4a09
Romulo Iorio <romuloiorio@hotmail.com>
2023-11-13 18:57:27 -0300

    feat: styling adjustments on MenuButton

--- app/components/PageLayout/MenuButton.tsx
+++ app/components/PageLayout/MenuButton.tsx
@@ -20,7 +20,7 @@ export const MenuButton: React.FC = () => {
 
   return (
     <div
-      className="absolute top-[2rem] md:lg:top-[1.25rem] lg:left-[1.25rem] left-[1rem] cursor-pointer lg:p-2 p-1 bg-[#90C7BE] hover:bg-[#62C8B8] rounded-md transition-all active:translate-y-1 z-[10]"
+      className="absolute top-[1rem] md:lg:top-[1.25rem] lg:left-[1.25rem] left-[1rem] cursor-pointer lg:p-2 p-1 bg-[#90C7BE] hover:bg-[#62C8B8] rounded-md transition-all active:translate-y-1 z-[10]"
       onClick={onClick}
     >
       <Icon className="text-black lg:text-[1.5rem] text-[1.25rem]" />
